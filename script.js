675cc483fb9014d04a84ad40fd72f6d8626d019e
Boburkhan <91623890+Boburkhanco@users.noreply.github.com>
2021-11-12 19:21:38 +0500

    commit

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ var input = document.querySelector("#input")
 var textDisplay = document.querySelector("#info")
 var submit = document.querySelector("#submit")
 
-
-
 submit.addEventListener('click' , e => {
-    e.preventDefault()
-    textDisplay.textContent = (input.value * 9 / 5 ) + 32
+        e.preventDefault()
+        if(scale.value == 'F') {
+            textDisplay.textContent = "Celcius darajada" + " " + (((input.value - 32) * 5 / 9).toFixed(3)) + "°C"
+        } else {
+            textDisplay.textContent = "Fahrenheit darajada" + " " + ((input.value * 9 / 5 ) + 32) + "°F"
+        }
 })
